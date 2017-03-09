4f74ed58d7486b459df2ae80683ee232ed5ddee5
Abishek KC <kcabishek@gmail.com>
2017-03-08 20:21:05 -0500

    Main Activity

---
+++ app/src/main/java/com/example/abishek/weatherupdate/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.abishek.weatherupdate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    final String URL_BASE = "http://api.openweathermap.org/data/2.5/forecast";
+    final String URL_COORD = "?lat=9.9687&lon=76.299";
+    final String URL_UNITS = "&units=imperial";
+    final String URL_API_KEY = "&APPID=af24039546e998bc1c5907e0352f3792";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final String URL = URL_BASE + URL_COORD + URL_UNITS + URL_API_KEY;
+
+        final JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, URL, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.v("Output", "Response: " +response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.v("Output", "Err: " + error.getLocalizedMessage());
+            }
+        });
+        Volley.newRequestQueue(this).add(jsonRequest);
+    }
+}
